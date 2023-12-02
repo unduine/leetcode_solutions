299fee34705d0e0fecaeb37a1b8f9ea0f0f85dcf
unduine <unduine@gmail.com>
2023-12-02 16:34:08 +0100

    issue 79

---
+++ 79.js
@@ -0,0 +1,43 @@
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+  const rows = board.length;
+  const cols = board[0].length;
+  let path = new Set();
+
+  const dfs = (r, c, i) => {
+    const currPath = `${r},${c}`;
+    console.log(i == word.length)
+    if (i == word.length) return true;
+
+    if (
+      r < 0 ||
+      c < 0 ||
+      r >= rows ||
+      c >= cols ||
+      word[i] !== board[r][c] ||
+      path.has(currPath)) {
+      return false;
+    }
+    path.add(currPath);
+    const res = dfs(r, c - 1, i + 1) ||
+      dfs(r, c + 1, i + 1) ||
+      dfs(r - 1, c, i + 1) ||
+      dfs(r + 1, c, i + 1);
+    path.delete(currPath);
+    return res;
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (dfs(i, j, 0)) return true;
+    }
+  }
+
+  return false;
+};
+
